0ed0e9e53a7daa2bdb3b39d2113e8d8bbd23e98b
Cong Fandi <cong.fandi@abersoft.se>
2021-04-27 11:40:53 +0700

    Initial Commit

---
+++ Lidemi/LidemiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidemiApp.swift
+//  Lidemi
+//
+//  Created by Cong Fandi on 27/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LidemiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
